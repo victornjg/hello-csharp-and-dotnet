6ad3cd3a1700e1da4a843b2ed8dbdf67afe07782
victornjg <victor.nnjg@gmail.com>
2021-05-05 21:25:13 -0300

    enums

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,7 +12,7 @@ namespace HelloWorld
             byte number = 255;
 
             // REALS NUMBERS ARE TREATED AS DOUBLE BY DEFAULT
-            //float f = 20.95;
+            var d = 20.95;
             float f = 20.95f;
 
             // LET THE C# COMPILER DETECT THE DATA TYPE FOR EACH VARIABLE
@@ -22,6 +22,11 @@ namespace HelloWorld
             // DIFFERENT QUOTATION MARKS FOR EACH TYPE
             char letter = 'a';
             string name = "Victor";
+
+            // ARRAY
+            //int[] someNumbers = new int[];  WRONG
+            int[] someNumbers1 = new int[] { 1, 2, 3 };
+            int[] someNumbers2 = new int[3];
 
             Console.WriteLine(number);
             Console.WriteLine(f);
@@ -40,6 +45,17 @@ namespace HelloWorld
 
             double multiplyRes = Calculator.Multiply(3, 4);
             Console.WriteLine("multiply result -> {0}", multiplyRes);
+
+            // ENUM
+            var questionLevel = QuestionLevelEnum.INTERMEDIATE;
+            Console.WriteLine("enum value -> {0}", (int) questionLevel);
+
+            var questionLevelId = 3;
+            Console.WriteLine("enum entry -> {0}", (QuestionLevelEnum) questionLevelId);
+
+            var questionLevelName = "BEGINNER";
+            var level = (QuestionLevelEnum) Enum.Parse(typeof(QuestionLevelEnum), questionLevelName);
+            Console.WriteLine("enum entry got from parse -> {0}", level);
         }
     }
 }
